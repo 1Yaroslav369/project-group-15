57b787c57a229cd40852d5cbcc41829384f50945
yaroslav kuzmin <kuzminyaroslav13@gmail.com>
2025-08-06 11:48:59 +0200

    add files and library

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// accordion
+import Accordion from "accordion-js";
+import "accordion-js/dist/accordion.min.css";
+// swiper
+import Swiper from 'swiper';
+import 'swiper/css'; 
+import 'swiper/css/pagination';
+import 'swiper/css/navigation'; 
+
+// iziToast
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
