b0a333b2df6cb435881c0b3fd7ca3bbaedee6e59
unknown <grishuna.julia@gmail.com>
2025-08-07 16:27:04 +0200

    corected html and css

--- src/js/furniture-list.js
+++ src/js/furniture-list.js
@@ -1,1 +1,12 @@
-const BASE_URL = 'https://furniture-store.b.goit.study/api';
+
+
+const API_URL = 'https://furniture-store.b.goit.study/api-docs';
+const categoryFilter = document.getElementById('categoryFilter');
+const furnitureList = document.getElementById('furnitureList');
+const loadMoreBtn = document.getElementById('loadMoreBtn');
+
+
+console.log('Запит відправлено');
+console.log('API_URL:', API_URL);
+
+console.log('URL:', `${API_URL}/categories`);
