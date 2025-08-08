faec57fb4370ecdaa13738c8babf0dafa82ba115
unknown <grishuna.julia@gmail.com>
2025-08-08 12:37:44 +0200

    last updates css

--- src/js/furniture-list.js
+++ src/js/furniture-list.js
@@ -2,21 +2,9 @@
 
 const BASE_URL = 'https://furniture-store.b.goit.study/api/';
 
-
+const categoryContainer = document.querySelector('.category-filter');
+const furnitureContainer = document.querySelector('.furniture-container');
 const loader = document.querySelector('.loader');
-
-
-
-
-fetch('https://furniture-store.b.goit.study/api/categories')
-  .then(res => {
-    if (!res.ok) throw new Error('Статус відповіді не ОК');
-    return res.json();
-  })
-  .then(data => console.log('Категорії:', data))
-  .catch(err => console.error('Помилка:', err));
-
-
 
 export function showLoader() {
      loader.classList.remove('hidden');
@@ -26,4 +14,11 @@ export function showLoader() {
 export function hideLoader() {
      loader.classList.add('hidden');
      
+}
+
+// Функція для отримання категорій
+async function fetchCategories() {
+  const res = await fetch(`${BASE_URL}categories`);
+  if (!res.ok) throw new Error('Помилка завантаження категорій');
+  return res.json();
 }
