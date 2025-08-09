de2b1d68b2fef4a9402a62e7e3798e33ac6c7890
Anastasiia Nepyipa <nastushkaaa23@gmail.com>
2025-08-09 23:04:39 +0200

    add FAQ-sectin

--- src/js/FAQ-section.js
+++ src/js/FAQ-section.js
@@ -1,2 +1,30 @@
 import Accordion from "accordion-js";
 import "accordion-js/dist/accordion.min.css";
+
+new Accordion('.accordion-container-FAQ', {
+  duration: 900,
+  showMultiple: false,
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const accordionItems = document.querySelectorAll('.FAQ-item');
+  const faqs = document.querySelectorAll('.FAQ-item');
+  faqs.forEach(faq =>
+    faq.addEventListener('click', () => {
+      faq.classList.toggle('active');
+    })
+  );
+
+  accordionItems.forEach(item => {
+    const btn = item.querySelector('.FAQ-btn');
+    const content = item.querySelector('.ac-panel');
+
+    btn.addEventListener('click', function () {
+      if (content.style.display === 'flex') {
+        content.style.overflow = 'hidden';
+      } else {
+        content.style.display = 'flex';
+      }
+    });
+  });
+});
